ee3e0f98e6b2533e97561347384a817dd5a11e7c
safa <safabelhouch@gmail.com>
2025-09-26 17:29:02 +0100

    preparing for vercel

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+
+
+// Projects images export
+export { default as project1 } from "./project-1.png";
+
+// Icons export (original set)
+export { default as sketch } from "./icons/sketch.svg";
+export { default as xd } from "./icons/xd.svg";
+export { default as tailwind } from "./icons/tailwind.svg";
+export { default as bootstrap } from "./icons/bootstrap.svg";
+export { default as react } from "./icons/react.svg";
+export { default as reactnative } from "./icons/reactnative.svg";
+export { default as emailicon } from "./icons/email.png";
+export { default as messengericon } from "./icons/messenger.png";
+export { default as whatsappicon } from "./icons/whatsapp.png";
+
+// Icons export (new set)
+export { default as css } from "./css.png";
+export { default as docker } from "./docker.png";
+export { default as figma } from "./figma.png"; 
+export { default as git } from "./git.png";
+export { default as html } from "./html.png";
+export { default as javascript } from "./javascript.png";
+export { default as mongodb } from "./mongodb.png";
+export { default as nodejs } from "./nodejs.png";
+export { default as reactjs } from "./reactjs.png";
+export { default as redux } from "./redux.png";
+export { default as typescript } from "./typescript.png";
+export { default as threejs } from "./threejs.svg";
+export { default as scss} from "./scss.png";
+export { default as gsap} from "./gsap.png";
+export { default as jwt} from "./jwt.png";
+export { default as apex} from "./apex.png";
